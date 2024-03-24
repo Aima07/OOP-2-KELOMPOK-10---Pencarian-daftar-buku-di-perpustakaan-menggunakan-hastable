44e8e4903b2914c7727d4804f05a3d70112490cd
ihsanfrr <ihsan.fajardan@gmail.com>
2024-03-24 21:57:50 +0700

    initial commit

---
+++ src/bookfinder/BookFinder.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bookfinder;
+
+/**
+ *
+ * @author ihsanfrr
+ */
+public class BookFinder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new HomeScreen().setVisible(true);
+    }
+    
+}
