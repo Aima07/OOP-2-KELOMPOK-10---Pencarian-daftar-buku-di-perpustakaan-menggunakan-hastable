694f5bb43f67e286ec81a9a3f3bb11703480b292
ihsanfrr <ihsan.fajardan@gmail.com>
2024-03-26 01:10:22 +0700

    added splash screen

--- src/bookfinder/BookFinder.java
+++ src/bookfinder/BookFinder.java
@@ -14,7 +14,7 @@ public class BookFinder {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new HomeScreen().setVisible(true);
+        new SplashScreen().setVisible(true);
     }
     
 }
